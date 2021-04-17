99e4e90642117383cc62f99c0512012b9440cf8b
sganzerla <alisson_sganzerla@yahoo.com.br>
2021-04-16 22:58:56 -0300

    refactor: exceptions catch

--- 04-Entendendo Exceções/ByteBank/ContaCorrente.cs
+++ 04-Entendendo Exceções/ByteBank/ContaCorrente.cs
@@ -4,10 +4,11 @@ namespace ByteBank
 {
     public class ContaCorrente
     {
-
         public static double TaxaOperacao { get; private set; }
         public static int TotalDeContasCriadas { get; private set; }
         public Cliente Titular { get; private set; }
+        public int ContadorSaquesNaoPermitidos { get; private set; }
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
         public int Agencia { get; }
         public int Numero { get; }
         private double _saldo = 100;
@@ -53,7 +54,10 @@ namespace ByteBank
                 throw new ArgumentException("Valor inválido para o saque.", nameof(valor));
 
             if (_saldo < valor)
+            {
+                ContadorSaquesNaoPermitidos++;
                 throw new SaldoInsuficienteException(Saldo, valor);
+            }
 
             Console.WriteLine("-----------------------------------------------------------------------------");
             Console.WriteLine("Sacando ... " + valor + " de " + Titular.Nome);
@@ -74,17 +78,24 @@ namespace ByteBank
 
         public void Transferir(double valor, ContaCorrente contaDestino)
         {
-
             if (valor < 0)
+            {
                 throw new ArgumentException("Valor inválido para tranferência.", nameof(valor));
+            }
 
-
-            Sacar(valor);
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+                throw;
+            }
             Console.WriteLine("-----------------------------------------------------------------------------");
             Console.WriteLine("Transferindo ... " + valor + " de " + Titular.Nome + " para " + contaDestino.Titular.Nome);
             Console.WriteLine("");
             contaDestino.Depositar(valor);
-
         }
 
         public void WriteLine()
--- 04-Entendendo Exceções/ByteBank/Program.cs
+++ 04-Entendendo Exceções/ByteBank/Program.cs
@@ -6,12 +6,8 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-
             Cliente gabriela = new Cliente("Gabriela", "09230293720", "Reporter");
             Cliente gabrielaCosta = new Cliente("Gabriela Costa", "2323123892", "Radialista");
-
-
-
             try
             {
                 ContaCorrente contaDaGabriela = new ContaCorrente(gabriela, 10, 222);
@@ -25,27 +21,27 @@ namespace ByteBank
                 contaDaGabriela.Depositar(200);
                 contaDaGabrielaCosta.WriteLine();
                 // contaDaGabriela.Transferir(500, contaDaGabrielaCosta); // erro
-                contaDaGabriela.Transferir(200, contaDaGabrielaCosta); 
+                contaDaGabriela.Transferir(200, contaDaGabrielaCosta);
                 contaDaGabriela.WriteLine();
                 contaDaGabrielaCosta.WriteLine();
-
             }
             catch (ArgumentException ex)
             {
                 Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
                 return;
             }
             catch (SaldoInsuficienteException ex)
             {
                 Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
                 return;
             }
-
-
-
-
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
-
         }
     }
 }
