22e18c0c14fcaeafc51a8ccdff298e54f6ed7e3e
sganzerla <alisson_sganzerla@yahoo.com.br>
2021-04-20 23:58:23 -0300

    chore: regex

--- 06-Strings, Expressões Regulares e a Classe Object/ByteBank/Program.cs
+++ 06-Strings, Expressões Regulares e a Classe Object/ByteBank/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
 namespace ByteBank
 {
     class Program
@@ -13,6 +13,18 @@ namespace ByteBank
 
             valor = extratorValor.GetValor("moedaDist");
             Console.WriteLine(valor);
+
+
+            // string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            // string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            // string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            string textoTeste = "meu número é 12324-4545";
+            Console.WriteLine("Padrão foi identificado? " + Regex.IsMatch(textoTeste, padrao));
+            Console.WriteLine("Valor é: " + Regex.Match(textoTeste, padrao).Value);
+
+
+            
             Console.ReadLine();
         }
 
